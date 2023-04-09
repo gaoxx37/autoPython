93aacdb8fc9107391a0da8136d24596433666af5
Mr. X <vimacser@outlook.com>
2023-04-09 11:49:24 +0800

    Update autoPy.py

--- autoPy.py
+++ autoPy.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-filename=datetime.now().strftime('%Y%m%d-%H:%M:%S')+'.txt'
+filename=datetime.now().strftime('%Y%m%d_%H_%M_%S')+'.txt'
 with open(filename, 'w', encoding='utf-8') as f:
     f.write(datetime.now().strftime('%Y%m%d-%H:%M:%S'))
